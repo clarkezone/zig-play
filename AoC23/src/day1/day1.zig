6dc67a8cae53cd8644372ceaae69acb830a8cb26
James Clarke <james@clarkezone.net>
2024-01-21 03:22:02 -0800

    restructure

---
+++ AoC23/src/day1/day1.zig
@@ -0,0 +1,5 @@
+const std = @import("std");
+
+pub fn day1() void {
+    std.debug.print("Hello.\n", .{});
+}
---
+++ AoC23/src/main.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const day1 = @import("day1/day1.zig");
+
+pub fn main() !void {
+    day1.day1();
+}
+
+test "simple test" {
+    var list = std.ArrayList(i32).init(std.testing.allocator);
+    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    try list.append(42);
+    try std.testing.expectEqual(@as(i32, 42), list.pop());
+}
