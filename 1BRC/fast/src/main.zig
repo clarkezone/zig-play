89d4d6a760672807b2d999f4b250b1b059833e84
James Clarke <james@clarkezone.net>
2024-03-02 20:54:16 -0800

    Printing improvements

--- 1BRC/fast/src/main.zig
+++ 1BRC/fast/src/main.zig
@@ -3,32 +3,43 @@ const std = @import("std");
 const scanstate = struct {
     start: u64,
     end: u64,
+    length: u64,
+    linecount: u64, //out
     file: std.fs.File,
     waitgroup: *std.Thread.WaitGroup,
 };
 
-fn scanfilesegment(state: *const scanstate) void {
-    state.*.waitgroup.start();
-    std.debug.print("Start\n", .{});
+fn scanfilesegment(state: *scanstate) void {
+    std.debug.print("Thread start: {} end: {}\n", .{ state.start, state.end });
     //TODO std.os.MAP.SHARED not working in 0.12
-    //TODO 1. replace length [x]
-    const mapper = std.os.mmap(null, state.*.end, std.os.PROT.READ, std.os.MAP.PRIVATE, state.*.file.handle, state.*.start) catch |err| {
+    //TODO can we map portions of the file to save memory?
+    //const mapper = std.os.mmap(null, state.*.end, std.os.PROT.READ, std.os.MAP.SHARED, state.*.file.handle, state.*.start) catch |err| {
+    const mapper = std.os.mmap(null, state.*.length, std.os.PROT.READ, std.os.MAP.SHARED, state.*.file.handle, 0) catch |err| {
         std.debug.print("Error: {}\n", .{err});
         return;
     };
-    //TODO 5. look at tokenizer and handle newline + ;
-    //TODO 4. use multiple threads
-    //TODO 3. use two fixed threads
+    //TODO 8. use n threads
+    //TODO 7. find mindpoint that is newline boundary
+    //TODO 6. look at tokenizer and handle newline + ;
+    //TODO 5. use argument for filepath and add a hyperfine test
+    //TODO 4. pass results back and add on main thread, ensure adds up to 1B [x]
+    //TODO 3. use two fixed threads [x]
     //TODO 2. loop and count all newlines [x]
     //TODO 2.5 add timer [x]
     var totallines: u64 = 0;
-    var returnpos = std.mem.indexOfScalarPos(u8, mapper, 0, '\n');
+    var returnpos = std.mem.indexOfScalarPos(u8, mapper, state.start, '\n');
     while (returnpos) |pos| {
-        totallines += 1;
-        returnpos = std.mem.indexOfScalarPos(u8, mapper, pos + 1, '\n');
+        if (pos < state.end) {
+            totallines += 1;
+            returnpos = std.mem.indexOfScalarPos(u8, mapper, pos + 1, '\n');
+        } else {
+            std.debug.print("Breaking at: {}\n", .{pos});
+            break;
+        }
     }
     defer std.os.munmap(mapper);
-    std.debug.print("Stop\n", .{});
+    state.*.linecount = totallines;
+    std.debug.print("Thread Stop with linecount {}\n", .{totallines});
     state.*.waitgroup.finish();
 }
 
@@ -39,22 +50,24 @@ pub fn scanfile() !void {
     var waitgroup = std.Thread.WaitGroup{};
 
     const filelen = try fileh.getEndPos();
-    const ss: scanstate = .{ .start = 0, .end = filelen / 2, .file = fileh, .waitgroup = &waitgroup };
-    //const ss2: scanstate = .{ .start = filelen / 2, .end = filelen, .file = fileh, .waitgroup = &waitgroup };
+    var ss: scanstate = .{ .start = 0, .end = filelen / 2, .length = filelen, .linecount = 0, .file = fileh, .waitgroup = &waitgroup };
+    var ss2: scanstate = .{ .start = filelen / 2, .end = filelen, .length = filelen, .linecount = 0, .file = fileh, .waitgroup = &waitgroup };
 
     var timer = try std.time.Timer.start();
     const th = try std.Thread.spawn(.{}, scanfilesegment, .{&ss});
-    //const th2 = try std.Thread.spawn(.{}, scanfilesegment, .{&ss2});
-    waitgroup.wait();
+    const th2 = try std.Thread.spawn(.{}, scanfilesegment, .{&ss2});
     defer th.join();
-    //defer th2.join();
+    defer th2.join();
+    waitgroup.start(); //BUG if thread doesn't start due to join immediately due to error
+    waitgroup.start();
+    waitgroup.wait();
 
     const elapsedSecs = @as(f32, @floatFromInt(timer.read())) / 1e9;
-    std.debug.print("\nFound: {} lines in {d:5} seconds\n", .{ 0, elapsedSecs });
+    std.debug.print("\nFound: {} lines in {d:5} seconds\n", .{ ss.linecount + ss2.linecount, elapsedSecs });
 }
 
 pub fn main() !void {
-    std.debug.print("Speed of light test running..", .{});
+    std.debug.print("Speed of light test running..\n", .{});
     try scanfile();
 }
 
